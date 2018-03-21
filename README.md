0c280b49a96135345d8a07adeaac919502dd0ef6
Ch3sh1r3Cat <ca.leduc@me.com>
2018-03-21 23:30:18 +0100

    Version 0.1.0
    vue-svgi
    A simple inline SVG component plugin for Vue.js

---
+++ README.md
@@ -0,0 +1,62 @@
+# vue-svgi
+
+A simple inline SVG component plugin for Vue.js
+
+## Installation
+
+```javascript
+npm install vue-svgi --save
+```
+
+## How to use
+
+Default use in your main.js Vue project
+
+```javascript
+import Icon from 'vue-svgi'
+
+Vue.use(Icon)
+```
+
+Load a family name with a json file for svg paths
+
+```javascript
+Vue.Icon.add('ico', require('./assets/icons/ico.json'))
+```
+
+The JSON value must be an array of three parameters: width (number), height (number), paths (string). You can separate multiple paths with '|'
+
+```javascript
+{
+  "arrow-right": [
+    256,
+    512,
+    "M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+  ],
+  ...
+}
+```
+
+You can use the <icon> component in your HTML template
+
+```javascript
+...
+<div>
+  <icon family="ico" name="app"></icon>
+</div>
+...
+```
+
+The rendered SVG element, created with the class 'svg-inline' can then be easily customized with CSS.
+
+```javascript
+...
+<div>
+  <svg aria-hidden="true" class="svg-inline ico ico-arrow-right" data-name="arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path class="path path-0" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z" fill="currentColor"></path></svg>
+</div>
+...
+```
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,93 @@
+/**
+ * @license
+ * Copyright (c) 2018 Charles-André LEDUC. All rights reserved.
+ */
+
+const Icon = {
+  install(Vue, options) {
+    var families = {}
+
+    Vue.component('icon', {
+      render: function(createElement) {
+        let values = this.values()
+        let paths = []
+        if (values.paths != null) {
+          if (values.paths.length > 0) {
+            for (var index in values.paths) {
+              let path = values.paths[index]
+              if (typeof path === 'string') {
+                paths.push(
+                  createElement('path', {
+                    attrs: {
+                      class: 'path path-' + index,
+                      d: path.trim(),
+                      fill: 'currentColor'
+                    }
+                  })
+                )
+              }
+            }
+          }
+        }
+        var classes = [this.family, this.family + '-' + this.name]
+        if (this.append != null) {
+          classes.push(this.append)
+        }
+        let data = {
+          attrs: {
+            'aria-hidden': 'true',
+            class: 'svg-inline ' + classes.join(' '),
+            'data-name': this.name,
+            role: 'img',
+            xmlns: 'http://www.w3.org/2000/svg',
+            viewBox: '0 0 ' + values.width + ' ' + values.height
+          }
+        }
+        return createElement('svg', data, paths)
+      },
+      props: {
+        family: {
+          type: String,
+          required: true
+        },
+        name: {
+          type: String,
+          required: true
+        },
+        append: {
+          type: String,
+          default: ''
+        }
+      },
+      methods: {
+        values: function() {
+          let family = families[this.family]
+          if (family != null) {
+            let values = family[this.name]
+            if (values != null && values.length === 3) {
+              let paths = values[2].split('|')
+              return {
+                width: values[0],
+                height: values[1],
+                paths: paths
+              }
+            }
+          }
+          return {
+            width: 0,
+            height: 0,
+            paths: []
+          }
+        }
+      }
+    })
+
+    Vue.Icon = {
+      load: function(name, json) {
+        families[name] = json
+      }
+    }
+  }
+}
+
+export default Icon
