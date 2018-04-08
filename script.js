010751b00c62c077cdd2d2a3f6c5fbb29ecb5544
Ch3sh1r3Cat <ca.leduc@me.com>
2018-04-08 23:39:41 +0200

    Version 0.1.1
    - Compiled index.js with Babel to add compatibility with IE11 or older

--- script.js
+++ script.js
@@ -3,33 +3,35 @@
  * Copyright (c) 2018 Charles-André LEDUC. All rights reserved.
  */
 
-var Icon = {
-  install: function install(Vue, options) {
-    var families = {};
+const Icon = {
+  install(Vue, options) {
+    var families = {}
 
     Vue.component('icon', {
-      render: function render(createElement) {
-        var values = this.values();
-        var paths = [];
+      render: function(createElement) {
+        var values = this.values()
+        var paths = []
         if (values.paths != null) {
           if (values.paths.length > 0) {
             for (var index in values.paths) {
-              var path = values.paths[index];
+              var path = values.paths[index]
               if (typeof path === 'string') {
-                paths.push(createElement('path', {
-                  attrs: {
-                    class: 'path path-' + index,
-                    d: path.trim(),
-                    fill: 'currentColor'
-                  }
-                }));
+                paths.push(
+                  createElement('path', {
+                    attrs: {
+                      class: 'path path-' + index,
+                      d: path.trim(),
+                      fill: 'currentColor'
+                    }
+                  })
+                )
               }
             }
           }
         }
-        var classes = [this.family, this.family + '-' + this.name];
+        var classes = [this.family, this.family + '-' + this.name]
         if (this.append != null) {
-          classes.push(this.append);
+          classes.push(this.append)
         }
         var data = {
           attrs: {
@@ -40,8 +42,8 @@ var Icon = {
             xmlns: 'http://www.w3.org/2000/svg',
             viewBox: '0 0 ' + values.width + ' ' + values.height
           }
-        };
-        return createElement('svg', data, paths);
+        }
+        return createElement('svg', data, paths)
       },
       props: {
         family: {
@@ -58,34 +60,34 @@ var Icon = {
         }
       },
       methods: {
-        values: function values() {
-          var family = families[this.family];
+        values: function() {
+          var family = families[this.family]
           if (family != null) {
-            var values = family[this.name];
+            var values = family[this.name]
             if (values != null && values.length === 3) {
-              var paths = values[2].split('|');
+              var paths = values[2].split('|')
               return {
                 width: values[0],
                 height: values[1],
                 paths: paths
-              };
+              }
             }
           }
           return {
             width: 0,
             height: 0,
             paths: []
-          };
+          }
         }
       }
-    });
+    })
 
     Vue.Icon = {
-      load: function load(name, json) {
-        families[name] = json;
+      load: function(name, json) {
+        families[name] = json
       }
-    };
+    }
   }
-};
+}
 
-export default Icon;
+export default Icon
